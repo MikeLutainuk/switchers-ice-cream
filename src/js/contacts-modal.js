4f295150b65a04f63323e2636cfb9e80a02e6be7
nikkuts <nickkuts888@gmail.com>
2022-12-18 17:05:22 +0200

    додає модальні вікна секції contacts

--- src/js/contacts-modal.js
+++ src/js/contacts-modal.js
@@ -1,14 +1,39 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
+    modal: document.querySelectorAll('[data-modal]'),
+    body: document.body,
+  };
+  // console.log(refs.openModalBtn);
+  // console.log(refs.closeModalBtn);
+  // console.log(refs.modal);
+  let index = 0;
+
+  for (let openBtn of refs.openModalBtn) {
+    openBtn.onclick = () => {
+      index = +openBtn.dataset.modalOpen;
+      openSomeModal(index);
     };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+
+    function openSomeModal(num) {
+      for (let i = 0; i < refs.modal.length; i++) {
+        refs.modal[i].classList.add('is-hidden');
+      }
+      refs.modal[num].classList.remove('is-hidden');
+      refs.body.classList.add('overflow');
     }
-  })();
+  }
+
+  for (let closeBtn of refs.closeModalBtn) {
+    closeBtn.onclick = () => {
+      index = +closeBtn.dataset.modalClose;
+      closeSomeModal(index);
+    };
+
+    function closeSomeModal(num) {
+      refs.modal[num].classList.add('is-hidden');
+      refs.body.classList.remove('overflow');
+    }
+  }
+})();
