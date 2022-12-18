3a0d27df9e4b56611f2483b92809acbdd2f8e49a
MikeLutainuk <113596204+MikeLutainuk@users.noreply.github.com>
2022-12-18 15:52:28 +0000

    Deploying to gh-pages from @ MikeLutainuk/switchers-ice-cream@d0e590267cdb157d62056378ee0e0b65eb63cceb 🚀

---
+++ index.a1797229.js
@@ -0,0 +1,2 @@
+const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.a1797229.js.map
