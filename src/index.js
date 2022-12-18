fbb9feddc5b7625559dd2ad5191add5d85aa9088
MikeLutainuk <113596204+MikeLutainuk@users.noreply.github.com>
2022-12-18 15:15:42 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './js/customer-reviews';
 import './js/header-modal';
 
 import './js/contacts-modal';
+import './js/about-modal';
+
 
 import './js/sliding-menu';
 import './js/mobile-menu';
--- src/js/contacts-modal.js
+++ src/js/contacts-modal.js
@@ -1,14 +1,14 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector("[data-modal-open='21']"),
+//       closeModalBtn: document.querySelector("[data-modal-close='21']"),
+//       modal: document.querySelector("[data-modal='21']"),
+//     };
   
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+//     refs.openModalBtn.addEventListener("click", toggleModal);
+//     refs.closeModalBtn.addEventListener("click", toggleModal);
   
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+//   function toggleModal() {
+//      refs.modal.classList.toggle("is-hidden");
+//     }
+//   })();
