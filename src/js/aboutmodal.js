1f3c4ab749e90d8eaf79934bf0b1a3444e084bf2
ovchrk <113932934+ovchrk@users.noreply.github.com>
2022-12-18 15:02:28 +0200

     script added

---
+++ src/js/aboutmodal.js
@@ -0,0 +1,39 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-aboutmodal-open]'),
+    closeModalBtn: document.querySelectorAll('[data-aboutmodal-close]'),
+    modal: document.querySelectorAll('[data-aboutmodal]'),
+    body: document.body,
+  };
+  // console.log(refs.openModalBtn);
+  // console.log(refs.closeModalBtn);
+  // console.log(refs.modal);
+  let index = 0;
+
+  for (let openBtn of refs.openModalBtn) {
+    openBtn.onclick = () => {
+      index = +openBtn.dataset.modalOpen;
+      openSomeModal(index);
+    };
+
+    function openSomeModal(num) {
+      for (let i = 0; i < refs.modal.length; i++) {
+        refs.modal[i].classList.add('is-hidden');
+      }
+      refs.modal[num].classList.remove('is-hidden');
+      refs.body.classList.add('overflow');
+    }
+  }
+
+  for (let closeBtn of refs.closeModalBtn) {
+    closeBtn.onclick = () => {
+      index = +closeBtn.dataset.modalClose;
+      closeSomeModal(index);
+    };
+
+    function closeSomeModal(num) {
+      refs.modal[num].classList.add('is-hidden');
+      refs.body.classList.remove('overflow');
+    }
+  }
+})();
