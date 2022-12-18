4b49caf063d0889101123d9f201bd2c48ca2c0da
MikeLutainuk <113596204+MikeLutainuk@users.noreply.github.com>
2022-12-18 18:34:17 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './js/galery';
 import './js/customer-reviews';
 import './js/header-modal';
 
-import './js/contacts-modal';
+import './js/franshise-modal';
 import './js/about-modal';
 
 import './js/swiper-slider';
