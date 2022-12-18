18ed8ca7cb341fdd7ace0b2623f620a4fb064fed
Neshodym <voinash@gmail.com>
2022-12-18 23:24:36 +0900

    js

--- src/js/modal-prod-menu.js
+++ src/js/modal-prod-menu.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-modal-open7]"),
+    closeModalBtn: document.querySelector("[data-modal-close7]"),
+    modal: document.querySelector("[data-modal7]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
