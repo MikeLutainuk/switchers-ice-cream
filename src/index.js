cc937adb80e4b9b41672ed81057a27e08552ecac
MikeLutainuk <113596204+MikeLutainuk@users.noreply.github.com>
2022-12-19 15:26:24 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@
 import './js/locations-modal';
 import './js/franshise-modal';
 import './js/about-modal';
-import './js/galery';
+import './js/gallery';
 import './js/swiper-slider';
 import './js/sliding-menu';
 import './js/mobile-menu';
 import './js/modal-header';
-// import './js/modal-prod-menu';
+import './js/modal-prod-menu';
