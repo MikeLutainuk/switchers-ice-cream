34b6cc07b0f2b35efca38034c037219c311a4ef1
Neshodym <voinash@gmail.com>
2022-12-20 16:29:29 +0900

    1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 
 
+import './js/galery';
+
+import './js/header-modal';
+
+
+import './js/sliding-menu';
+
+
+
+
+
 import './js/locations-modal';
 import './js/franshise-modal';
 import './js/about-modal';
@@ -7,5 +18,6 @@ import './js/gallery';
 import './js/swiper-slider';
 import './js/sliding-menu';
 import './js/mobile-menu';
+
 import './js/modal-header';
 import './js/modal-prod-menu';
