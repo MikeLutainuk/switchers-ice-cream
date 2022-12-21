0743b2ef679d6595c5f2ed41b0d1262410b9777c
MikeLutainuk <113596204+MikeLutainuk@users.noreply.github.com>
2022-12-21 07:50:17 +0000

    Deploying to gh-pages from @ MikeLutainuk/switchers-ice-cream@4ede673b17bfbfabb28c5075ad9ff5891f102d61 🚀

---
+++ index.ec5d1533.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open='12']"),closeModalBtn:document.querySelector("[data-modal-close='12']"),modal:document.querySelector("[data-modal='12']")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.ec5d1533.js.map
