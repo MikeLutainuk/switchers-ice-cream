4a6eff4a74935509204b14c613d1ae944336de49
MikeLutainuk <113596204+MikeLutainuk@users.noreply.github.com>
2022-12-22 14:04:51 +0200

    up

--- src/js/modal-ingredients.js
+++ src/js/modal-ingredients.js
@@ -1,14 +1,14 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open='111']"),
-    closeModalBtn: document.querySelector("[data-modal-close='111']"),
-    modal: document.querySelector("[data-modal='111']"),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open='111']"),
+//     closeModalBtn: document.querySelector("[data-modal-close='111']"),
+//     modal: document.querySelector("[data-modal='111']"),
+//   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
